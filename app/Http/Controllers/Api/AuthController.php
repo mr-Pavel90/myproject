46a6148e09d479b314214176e6569adda93479ae
Привет! <p.d.090909.m@gmail.com>
2025-11-30 20:10:16 +0200

    add token to api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Mail\WelcomeMail;
 use Illuminate\Support\Facades\Mail;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
@@ -26,13 +27,16 @@ class AuthController extends Controller
 
         $user = Auth::user();
 
+        $token = $user->createToken('api-token')->plainTextToken;
+
         return response()->json([
             'message' => 'User logged in successfully',
             'data'    => $user,
+            'token'   => $token
         ]);
     }
 
-    public function register(RegisterRequest $request)
+    public function register(RegisterRequest $request)  
     {
         $data = $request->validated();
 
@@ -43,15 +47,21 @@ class AuthController extends Controller
             'password' => Hash::make($data['password']),
         ]);
 
-         Mail::to($user->email)->send(new WelcomeMail($user));   
+        $token = $user->createToken('api-token')->plainTextToken;
 
-    return response()->json(['message' => 'User registered and email sent']);
-        
+        Mail::to($user->email)->send(new WelcomeMail($user));
+
+        return response()->json([
+            'message' => 'User registered and email sent',
+            'token'   => $token
+        ]);  
     }
 
      public function logout(Request $request)
     {
         Auth::logout();
+
+        $request->user()->currentAccessToken()->delete();
 
         return response()->json([
             'message' => 'User logged out successfully',
--- routes/api.php
+++ routes/api.php
@@ -15,29 +15,27 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-// user
-Route::get('users', [\App\Http\Controllers\Api\UserController::class, 'index']);
-Route::post('users', [\App\Http\Controllers\Api\UserController::class, 'store']);
-Route::delete('users/{id}', [\App\Http\Controllers\Api\UserController::class, 'delete']);
-Route::put('users/{id}', [\App\Http\Controllers\Api\UserController::class, 'update']);
-Route::get('users/{id}', [\App\Http\Controllers\Api\UserController::class, 'getUser']);
-
-// weather
-Route::get('weather', [\App\Http\Controllers\Api\WeatherController::class, 'getWeather']);
-Route::get('/weather/{id}', [\App\Http\Controllers\Api\WeatherController::class, 'getOne']);
-Route::post('weather', [\App\Http\Controllers\Api\WeatherController::class, 'store']);
-Route::delete('weather/{id}', [\App\Http\Controllers\Api\WeatherController::class, 'delete']);
-Route::put('weather/{id}', [\App\Http\Controllers\Api\WeatherController::class, 'update']);
-
-// Login
+ // Login
 Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
 // Register
 Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
 
-// Logout
-Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
+    // user
+    Route::get('users', [\App\Http\Controllers\Api\UserController::class, 'index']);
+    Route::post('users', [\App\Http\Controllers\Api\UserController::class, 'store']);
+    Route::delete('users/{id}', [\App\Http\Controllers\Api\UserController::class, 'delete']);
+    Route::put('users/{id}', [\App\Http\Controllers\Api\UserController::class, 'update']);
+    Route::get('users/{id}', [\App\Http\Controllers\Api\UserController::class, 'getUser']);
+
+    // weather
+    Route::get('weather', [\App\Http\Controllers\Api\WeatherController::class, 'getWeather']);
+    Route::get('/weather/{id}', [\App\Http\Controllers\Api\WeatherController::class, 'getOne']);
+    Route::post('weather', [\App\Http\Controllers\Api\WeatherController::class, 'store']);
+    Route::delete('weather/{id}', [\App\Http\Controllers\Api\WeatherController::class, 'delete']);
+    Route::put('weather/{id}', [\App\Http\Controllers\Api\WeatherController::class, 'update']);
+
+    // Logout
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
